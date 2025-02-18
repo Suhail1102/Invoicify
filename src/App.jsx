4f5c9679da79a170cd962194ed6cf136d3855bdc
Mohammad Suhail <mohdsuhail2762@gmail.com>
2025-02-18 13:51:58 +0530

    Add NotFoundPage

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
-import { useState , useEffect } from 'react'
+import { useState, useEffect } from 'react'
 import './App.css'
-import { BrowserRouter, Routes, Route } from "react-router";
+import { BrowserRouter, Routes, Route, useLocation } from "react-router";
 import HomePage from './Pages/HomePage';
 import About from './Pages/About'
 import LoginPage from './Pages/LoginPage';
@@ -11,26 +11,39 @@ import { Navigate } from 'react-router-dom';
 import PrivateRoute from './Authentication/PrivateRoute';
 import { AuthContext } from './Authentication/AuthContext';
 
-function App() {
-  const { user }= useContext(AuthContext);
 
-  
+const NotFoundPage = () => {
+  let location = useLocation();
+
+  console.log("Page not found");
+  console.log({location});
+
+  return (
+    <h1 className='text-3xl'>Page not found</h1>
+  )
+}
+
+function App() {
+  const { user } = useContext(AuthContext);
+
+
   return (
     <>
-    
-    <BrowserRouter>
-    
-    <div className='dark:bg-zinc-900 dark:text-white transition-all'>
-      <Routes>
-        <Route path='/' element={<HomePage/>}/>
-        <Route path='/login' element={user ? <Navigate to="/"/> : <LoginPage />}/>
-        <Route path='/signup' element={<SignupPage/>}/>
-        <Route path='/invoice/form' element={ <PrivateRoute> <InvoicePage/></PrivateRoute>}/>
-    
-      {/* <TaxInvoiceOutput  />   */}
-   </Routes>
-  </div>
-  </BrowserRouter>
+
+      <BrowserRouter>
+
+        <div className='dark:bg-zinc-900 dark:text-white transition-all'>
+          <Routes>
+            <Route path='/' element={<HomePage />} />
+            <Route path='/login' element={user ? <Navigate to="/" /> : <LoginPage />} />
+            <Route path='/signup' element={<SignupPage />} />
+            <Route path='/invoice/form' element={<PrivateRoute> <InvoicePage /></PrivateRoute>} />
+            <Route component={NotFoundPage} />
+
+            {/* <TaxInvoiceOutput  />   */}
+          </Routes>
+        </div>
+      </BrowserRouter>
 
     </>
   )
